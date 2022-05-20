5b2712920e2c63cb29489ce4599959f8dbb62bb4
st2diio <st2diio@protonmail.ch>
2022-05-20 14:41:42 +0200

    feat: fix config

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -48,24 +48,24 @@ function WalletButton() {
 
 function App() {
   // Read more about useDapp on https://usedapp.io/
-  const { error: contractCallError, value: tokenBalance } =
-    useCall({
-       contract: new Contract(addresses.ceaErc20, abis.erc20),
-       method: "balanceOf",
-       args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
-    }) ?? {};
+  // const { error: contractCallError, value: tokenBalance } =
+  //   useCall({
+  //      contract: new Contract(addresses.ceaErc20, abis.erc20),
+  //      method: "balanceOf",
+  //      args: ["0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C"],
+  //   }) ?? {};
 
-  const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
+  // const { loading, error: subgraphQueryError, data } = useQuery(GET_TRANSFERS);
 
-  useEffect(() => {
-    if (subgraphQueryError) {
-      console.error("Error while querying subgraph:", subgraphQueryError.message);
-      return;
-    }
-    if (!loading && data && data.transfers) {
-      console.log({ transfers: data.transfers });
-    }
-  }, [loading, subgraphQueryError, data]);
+  // useEffect(() => {
+  //   if (subgraphQueryError) {
+  //     console.error("Error while querying subgraph:", subgraphQueryError.message);
+  //     return;
+  //   }
+  //   if (!loading && data && data.transfers) {
+  //     console.log({ transfers: data.transfers });
+  //   }
+  // }, [loading, subgraphQueryError, data]);
 
   return (
     <Container>
