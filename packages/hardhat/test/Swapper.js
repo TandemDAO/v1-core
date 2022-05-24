5f8422c4944274645c022fb30a79398dc2806387
william godard <grenier.godard@gmail.com>
2022-05-24 17:52:50 +0200

    feat: add vesting and tests

--- packages/hardhat/test/Swapper.js
+++ packages/hardhat/test/Swapper.js
@@ -1,7 +1,6 @@
 const { expect } = require('chai')
 const { assert } = require('console')
 const { ethers } = require('hardhat')
-const { isRegExp } = require('lodash')
 
 let owner, proposer, holderA, holderB
 let swapper, tokenA, tokenB
@@ -82,7 +81,7 @@ describe('Swapper', function () {
     })
 
     it('should be able to cancel after the deadline is reached', async () => {
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < 6; i++) {
         await hre.ethers.provider.send('evm_mine')
       }
 
@@ -146,54 +145,128 @@ describe('Swapper', function () {
     })
   })
 
-  describe('claim swap', function () {
-    beforeEach(async () => {
-      await swapper
-        .connect(proposer)
-        .propose(
-          holderA.address,
-          tokenA.address,
-          tokenASwapAmount,
-          holderB.address,
-          tokenB.address,
-          tokenBSwapAmount,
-          10,
-          4,
-        )
+  describe('claim tokens', () => {
+    describe('when deal is Pending', () => {
+      beforeEach(async () => {
+        await swapper
+          .connect(proposer)
+          .propose(
+            holderA.address,
+            tokenA.address,
+            tokenASwapAmount,
+            holderB.address,
+            tokenB.address,
+            tokenBSwapAmount,
+            0,
+            4,
+          )
+        await tokenA.connect(holderA).approve(swapper.address, tokenASwapAmount)
+        await tokenB.connect(holderB).approve(swapper.address, tokenBSwapAmount)
+        await swapper.connect(holderA).approve(0)
+      })
+      it('should revert if status is pending', async () => {
+        await expect(swapper.connect(holderA).claim(0)).to.be.revertedWith('Swapper: deal must be Approved')
+      })
     })
 
-    it('cannot claim a proposal which has not been approved', async () => {
-      await expect(swapper.claim(0)).to.be.revertedWith('Swapper: the deal has not been approved by both parties')
+    describe('when deadline not past', () => {
+      beforeEach(async () => {
+        await swapper
+          .connect(proposer)
+          .propose(
+            holderA.address,
+            tokenA.address,
+            tokenASwapAmount,
+            holderB.address,
+            tokenB.address,
+            tokenBSwapAmount,
+            2,
+            10,
+          )
+        await tokenA.connect(holderA).approve(swapper.address, tokenASwapAmount)
+        await tokenB.connect(holderB).approve(swapper.address, tokenBSwapAmount)
+        await swapper.connect(holderA).approve(0)
+        await swapper.connect(holderB).approve(0)
+      })
+      it('should revert because deadline is not past', async () => {
+        await expect(swapper.connect(holderA).claim(0)).to.be.revertedWith('Swapper: deadline is not past')
+      })
     })
 
-    it('cannot claim before the vesting period ends', async () => {
-      await tokenA.connect(holderA).approve(swapper.address, tokenASwapAmount)
-      await tokenB.connect(holderB).approve(swapper.address, tokenBSwapAmount)
-      await swapper.connect(holderA).approve(0)
-      await swapper.connect(holderB).approve(0)
-
-      await expect(swapper.claim(0)).to.be.revertedWith('Swapper: vesting period is not over')
-    })
-
-    it('should perform token swap after the vesting period of an approved proposal', async () => {
-      await tokenA.connect(holderA).approve(swapper.address, tokenASwapAmount)
-      await tokenB.connect(holderB).approve(swapper.address, tokenBSwapAmount)
-      await swapper.connect(holderA).approve(0)
-      await swapper.connect(holderB).approve(0)
-
-      for (let i = 0; i < 10; i++) {
+    describe('when vesting is 0', () => {
+      beforeEach(async () => {
+        await swapper
+          .connect(proposer)
+          .propose(
+            holderA.address,
+            tokenA.address,
+            tokenASwapAmount,
+            holderB.address,
+            tokenB.address,
+            tokenBSwapAmount,
+            0,
+            4,
+          )
+        await tokenA.connect(holderA).approve(swapper.address, tokenASwapAmount)
+        await tokenB.connect(holderB).approve(swapper.address, tokenBSwapAmount)
+        await swapper.connect(holderA).approve(0)
+        await swapper.connect(holderB).approve(0)
+      })
+      it('should revert if caller is not one the accounts', async () => {
+        await expect(swapper.connect(proposer).claim(0)).to.be.revertedWith('Swapper: caller not allowed')
+      })
+      it(`should return the full tokenB amount (${tokenBSwapAmount})`, async () => {
         await hre.ethers.provider.send('evm_mine')
-      }
+        expect(await swapper.connect(holderA).callStatic.claim(0)).to.equal(tokenBSwapAmount)
+      })
+      it(`should return the full tokenA amount (${tokenASwapAmount})`, async () => {
+        await hre.ethers.provider.send('evm_mine')
+        expect(await swapper.connect(holderB).callStatic.claim(0)).to.equal(tokenASwapAmount)
+      })
+      it('should emit the dealClaimed event', async () => {
+        await swapper.connect(holderA).claim(0)
 
-      await swapper.claim(0)
+        await expect(swapper.connect(holderB).claim(0)).to.emit(swapper, 'DealClaimed').withArgs(0, holderB.address)
+      })
+    })
 
-      // Holders now own each other's tokens
-      expect(await tokenA.balanceOf(holderB.address)).to.equal(tokenASwapAmount)
-      expect(await tokenB.balanceOf(holderA.address)).to.equal(tokenBSwapAmount)
+    describe('when vesting is > 0', () => {
+      beforeEach(async () => {
+        await swapper
+          .connect(proposer)
+          .propose(
+            holderA.address,
+            tokenA.address,
+            tokenASwapAmount,
+            holderB.address,
+            tokenB.address,
+            tokenBSwapAmount,
+            5,
+            5,
+          )
+        await tokenA.connect(holderA).approve(swapper.address, tokenASwapAmount)
+        await tokenB.connect(holderB).approve(swapper.address, tokenBSwapAmount)
+        await swapper.connect(holderA).approve(0)
+        await swapper.connect(holderB).approve(0)
+      })
+      it(`should return the vested amount for first block ${tokenASwapAmount / 5}`, async () => {
+        // 4 blocks + 1 block mined = 5 blocks
+        for (let i = 0; i < 2; i++) {
+          await hre.ethers.provider.send('evm_mine')
+        }
+        expect(await swapper.connect(holderB).callStatic.claim(0)).to.equal(tokenASwapAmount / 5)
+      })
+      it(`should transfer ${tokenBSwapAmount / 5} tokens within ERC20`, async () => {
+        await hre.ethers.provider.send('evm_mine')
 
-      // Contract's token balance is zero
-      expect(await tokenA.balanceOf(swapper.address)).to.equal(0)
-      expect(await tokenB.balanceOf(swapper.address)).to.equal(0)
+        await swapper.connect(holderA).claim(0)
+
+        // Holders now own each other's tokens
+        expect(await tokenB.balanceOf(holderA.address)).to.equal(tokenBSwapAmount / 5)
+
+        // Contract's token balance is zero
+        expect(await tokenB.balanceOf(swapper.address)).to.equal(tokenBSwapAmount - tokenBSwapAmount / 5)
+      })
     })
   })
 })
